54eca9ee9877e9e4d50aff4fb0752af196052795
vdinesh2461990 <56682598+vdinesh2461990@users.noreply.github.com>
2019-11-04 19:55:00 +0530

    Update SimpleModel.py

--- models/simple-model/SimpleModel.py
+++ models/simple-model/SimpleModel.py
@@ -1,6 +1,9 @@
 class SimpleModel(object):
     def __init__(self):
         print("Initializing")
+        while True:
+            #num = int(input("Enter an integer: "))
+            print("Infinite Loop")
 
     def predict(self,X,features_names):
         return [[0.06]]
